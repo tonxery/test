079f864d01a3fd762ad67ed7d61df323721a84f5
tonxery <tongxinmiao@126.com>
2024-05-09 21:09:41 +0800

    add

--- test.py
+++ test.py
@@ -107,6 +107,125 @@ ic_priceticks = {
     "rb2101": 1,
     "600036": 5.8
 }
+#tewst
+
+
 
 contract_priceticks.update(ic_priceticks) #更新字典
 print(contract_priceticks)
+
+a = set()
+print(a)
+b = {1,2,3,4,5}
+print(b,type(b))
+a.add(10)
+a.add(8)
+a.remove(8)
+b.update({6,7,8,9,10})
+for i in  b:
+    print(i)
+print(b)
+if 5 in b:
+    print("True")
+b.clear()
+print(b)
+if_symblos = {"IF2009","IF2010"}
+ih_symbols = {"IH2009","IH2010"}
+new_symbols = if_symblos.union(ih_symbols)
+print(new_symbols)
+print(if_symblos,ih_symbols)
+print("---------")
+# if_symblos.insersection(ih_symbols)
+print(if_symblos.difference(ih_symbols))
+
+a1 = tuple()
+b1 = (1,2,3,4,5)
+print(b1)
+print(b1[0])
+# b1[0] = 7
+print(b1)
+for i in b1:
+    print(i)
+c1  = (8,)
+print(c1)
+l = [1,2,3,4,5,6]
+e = tuple(l)
+print(e)
+
+info_d = {
+    "symbol": "IF2009",
+    "direction": "Buy",
+    "price": 3800,
+    "volume": 1
+}
+symbol = info_d["symbol"]
+dirction = info_d["direction"]
+price = info_d["price"]
+volume = info_d["volume"]
+
+print(f"{symbol}发出委托，  {dirction} {volume}手@{price}")
+
+info_t = ("IF2009","Buy",3800,1)
+symbol,directon1,pricel,volume1 = info_t
+print(symbol,directon1,pricel,volume1)
+
+symbol2, _, price2, volume2 = info_t
+print(symbol2,volume2)
+
+symbol3,  *_, volume3 = info_t
+print("---")
+print(symbol3,_ , volume3)
+
+bars = []
+for i in range(1,5):
+    bar = {
+        "datatime": f"2020090{i}",
+        "open": (100 +i) * 100,
+        "high": (100 +i) * 100,
+        "low": (100 +i) * 100,
+        "close": (100 +i) * 100,
+    }
+    bars.append(bar)
+print(bars)
+print("--------------")
+for bar in bars:
+    print(bar)
+
+last_bar = bars[-1]
+print(last_bar)
+
+i = 5
+new_bar = {
+        "datatime": f"2020090{i}",
+        "open": (100 +i) * 100,
+        "high": (100 +i) * 100,
+        "low": (100 +i) * 100,
+        "close": (100 +i) * 100,
+    }
+bars.pop(0)
+bars.append(new_bar)
+print("-------------")
+for bar in bars:
+    print(bar)
+
+active_orderids = {}
+strategy_name = "demo_strategy"
+active_orderids["strategy_name"] = "strategy_orderids"
+print("============")
+print(active_orderids)
+
+d = {}
+# l = [1,2,3]
+# t = (5,5,8)
+# d[l[0]] = 1
+# print(d)
+# s1 = set()
+# print(type(s1))
+# s1.add(l)
+# print(s1)
+
+d[123] = l
+tp = (1,2,3)
+print(tp)
+tp = (1,2,l)
+print(tp)
